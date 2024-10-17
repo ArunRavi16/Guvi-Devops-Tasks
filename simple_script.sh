bd8c4fce530fe356791c11064ca3c1ef299ee118
ArunRavi16 <arunravichandran.196@gmail.com>
2024-10-17 17:18:39 +0530

    Create simple_script.sh

---
+++ simple_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script to display basic details and current date
+
+# Display basic details
+echo "-------------------------------"
+echo " Name: Arun Ravichandran"
+echo " Age: 27"
+echo " Nationality: Indian"
+echo "-------------------------------"
+
+# Display current date and time
+echo "Current Date and Time: $(date)"
